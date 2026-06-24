e7eb63301401a3ca401b448257f7d661ef8dd826
Abhijeet Gawali <gawaliabhijeet-cell>
2026-06-24 20:35:35 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import InputBox from "./componets/InputBox";
 import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState();
   const [from, setFrom] = useState("usd");
   const [to, setTo] = useState("inr");
   const [convertedAmount, setConvertedAmount] = useState(0);
